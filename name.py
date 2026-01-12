b8b4b97005a320cdc1941006bb67d4d0207f1116
dipto tamal <diptot90@gmail.com>
2026-01-13 01:18:34 +0600

    Add files via upload

---
+++ name.py
@@ -0,0 +1,12 @@
+# This is a beginner Python program
+
+name = input("tamal: ")
+age = input("tamal: ")
+
+print("Hello", name)
+print("You are", age, "years old")
+
+if int(age) >= 18:
+    print("You are an adult")
+else:
+    print("You are a minor")
