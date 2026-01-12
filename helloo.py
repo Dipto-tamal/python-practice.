90d2af1d47f7b48e5600c9d66c1d739a727d4a3e
dipto tamal <diptot90@gmail.com>
2026-01-13 01:18:11 +0600

    Add files via upload

---
+++ helloo.py
@@ -0,0 +1,13 @@
+# This is a beginner Python program
+
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+
+print("Hello", name)
+print("You are", age, "years old")
+
+if int(age) >= 18:
+    print("You are an adult")
+else:
+    print("You are a minor")
+
